223e6b550f7b47d3e649b3cf4be6cec84b90a633
Tony Truong <cwanstrath+hubot@gmail.com>
2023-01-26 23:21:26 +0100

    initial project setup

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ardanlabs/conf"
+)
+
+func main() {
+	fmt.Println("Hello world!")
+	conf.New()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/tonytrg/moduletest
+
+go 1.19
+
+require github.com/ardanlabs/conf v1.5.0
